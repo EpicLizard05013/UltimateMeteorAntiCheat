a1e3a0a1c05aa2e67d8ccf72f312ee46bf81776a
EpicLizard05013 <bailinwu610@gmail.com>
2026-09-12 19:15:20 -0400

    Refactor InventoryMoveCheck to use InventoryType import

--- src/main/java/net/meteorsmp/anticheat/checks/InventoryMoveCheck.java
+++ src/main/java/net/meteorsmp/anticheat/checks/InventoryMoveCheck.java
@@ -5,8 +5,8 @@ import net.meteorsmp.anticheat.manager.ViolationManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.InventoryType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class InventoryMoveCheck implements Listener {
