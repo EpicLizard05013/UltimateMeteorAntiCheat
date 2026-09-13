b838da7320d9f9e3594a985195abb22180ca1400
EpicLizard05013 <bailinwu610@gmail.com>
2026-09-13 14:38:37 -0400

    Create TridentDupeGuard class

---
+++ src/main/java/net/meteorsmp/anticheat/checks/TridentDupeGuard.java
@@ -0,0 +1,14 @@
+package net.meteorsmp.anticheat.checks;
+
+import net.meteorsmp.anticheat.UltimateMeoterAnticheat;
+import org.bukkit.event.Listener;
+
+public class TridentDupeGuard implements Listener {
+    private final UltimateMeoterAnticheat plugin;
+
+    public TridentDupeGuard(UltimateMeoterAnticheat plugin) {
+        this.plugin = plugin;
+    }
+    
+    // Add your Trident dupe detection logic here
+}
