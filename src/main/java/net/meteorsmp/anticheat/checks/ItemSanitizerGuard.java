f054f512255b5f83fa5714894ec70151e9611777
EpicLizard05013 <bailinwu610@gmail.com>
2026-09-13 14:38:33 -0400

    Create ItemSanitizerGuard class

---
+++ src/main/java/net/meteorsmp/anticheat/checks/ItemSanitizerGuard.java
@@ -0,0 +1,14 @@
+package net.meteorsmp.anticheat.checks;
+
+import net.meteorsmp.anticheat.UltimateMeoterAnticheat;
+import org.bukkit.event.Listener;
+
+public class ItemSanitizerGuard implements Listener {
+    private final UltimateMeoterAnticheat plugin;
+
+    public ItemSanitizerGuard(UltimateMeoterAnticheat plugin) {
+        this.plugin = plugin;
+    }
+    
+    // Add your item sanitization logic here
+}
