3de6265a900ed8211862a3432a26ef943a17b798
EpicLizard05013 <bailinwu610@gmail.com>
2026-09-13 14:28:42 -0400

    Add ContainerDupeGuard class to fix compilation error

---
+++ src/main/java/net/meteorsmp/anticheat/checks/ContainerDupeGuard.java
@@ -0,0 +1,12 @@
+package net.meteorsmp.anticheat.checks;
+
+import net.meteorsmp.anticheat.UltimateMeoterAnticheat;
+import org.bukkit.event.Listener;
+
+public class ContainerDupeGuard implements Listener {
+    private final UltimateMeoterAnticheat plugin;
+    
+    public ContainerDupeGuard(UltimateMeoterAnticheat plugin) {
+        this.plugin = plugin;
+    }
+}
